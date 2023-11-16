49ce6661a1338f8cb9fb499eb316ea9db90fd267
nguyenvancongdev <congthang.nuce@gmail.com>
2023-11-16 21:07:08 +0700

    rr

---
+++ App.jsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+
+
+
+
+
+
+
+function App() {
+  
+
+
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+
+
+export default App;
