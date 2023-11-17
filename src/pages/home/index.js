2b96965af391f2f5faa7de9f8cbe32ee26d5b2e1
congnv <congnv@etc.vn>
2023-11-17 09:03:04 +0700

    update

--- src/pages/home/index.js
+++
@@ -1,8 +0,0 @@
-import { View, Text} from 'react-native'
-export default HomeScreen = () => {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,4 @@
+import { View, Text } from 'react-native';
+export default () => {
+    <Text>4444</Text>
+}
